6c129031f771eb0926d76286f4df66cb632464ed
jonathan_uber <jonathanuber121207@gmail.com>
2026-02-26 19:48:57 -0300

    fix: refactor buttons order and add some validation in calculator

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -25,5 +25,16 @@ function ButtonOperation({label, onClick}: ButtonProps){
     )
 }
 
+function ButtonCalculate({label, onClick}: ButtonProps){
+    return (
+        <button 
+        className="bg-green-700 hover:bg-green-800 rounded p-4 transition text-white font-medium text-xl row-span-2"
+        onClick={onClick}
+        >
+            {label}
+        </button>
+    )
+}
+
 export default ButtonNumber;
-export { ButtonOperation };
+export { ButtonOperation, ButtonCalculate };
--- src/components/calculador.tsx
+++ src/components/calculador.tsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
 import Display from "./display";
-import ButtonNumber, { ButtonOperation } from "./button";
+import ButtonNumber, { ButtonOperation, ButtonCalculate } from "./button";
+import { evaluate } from 'mathjs';
 
 function Calculator(){
     const [display, setDisplay] = useState<string>("0");
+    const operators: string[] = ["+", "-", "/", "*", ".", "(", ")"] 
 
     const handleNumber = (num: string) => {
         setDisplay(display === "0" ? num : display + num);
     }
 
     const handleOperation = (op: string) => {
-        setDisplay(display === "0" ? "0" : display + op)
+        const lastChar = display.trim().slice(-1);
+        if(operators.includes(lastChar)){
+            return;
+        }
+
+        setDisplay(display === "0" ? "0" : display + op);
     }
     
+    const handleCalculate = (calc: string) =>{
+        setDisplay(String(evaluate(calc).toFixed(3)))
+    }
+
     return (
         <div className="flex flex-col items-center gap-4 mt-10 w-1/8 justify-self-center">
             <Display value={display} />
 
             <div className="grid grid-cols-4 gap-2 w-full">
+                <ButtonOperation label="+" onClick={ () => handleOperation("+")} />
+                <ButtonOperation label="-" onClick={ () => handleOperation("-")} />
+                <ButtonOperation label="X" onClick={ () => handleOperation("*" )} />
+                <ButtonOperation label="/" onClick={ () => handleOperation("/")} />
                 <ButtonNumber label="1" onClick={ () => handleNumber("1")} />
                 <ButtonNumber label="2" onClick={ () => handleNumber("2")} />
                 <ButtonNumber label="3" onClick={ () => handleNumber("3")} />
@@ -25,15 +40,14 @@ function Calculator(){
                 <ButtonNumber label="4" onClick={ () => handleNumber("4")} />
                 <ButtonNumber label="5" onClick={ () => handleNumber("5")} />
                 <ButtonNumber label="6" onClick={ () => handleNumber("6")} />
-                <ButtonOperation label="+" onClick={ () => handleOperation(" + ")} />
+                <ButtonCalculate label="=" onClick={ () => handleCalculate(display)} />
                 <ButtonNumber label="7" onClick={ () => handleNumber("7")} />
                 <ButtonNumber label="8" onClick={ () => handleNumber("8")} />
                 <ButtonNumber label="9" onClick={ () => handleNumber("9")} />
-                <ButtonOperation label="-" onClick={ () => handleOperation(" - ")} />
                 <ButtonNumber label="." onClick={ () => handleNumber(".")} />
                 <ButtonNumber label="0" onClick={ () => handleNumber("0")} />
-                <ButtonOperation label="/" onClick={ () => handleOperation(" / ")} />
-                <ButtonOperation label="X" onClick={ () => handleOperation(" *" )} />
+                <ButtonOperation label="(" onClick={ () => handleOperation("(")} />
+                <ButtonOperation label=")" onClick={ () => handleOperation(")")} />
             </div>
 
         </div>
